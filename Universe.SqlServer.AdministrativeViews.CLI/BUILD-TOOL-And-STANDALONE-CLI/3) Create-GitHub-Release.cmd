d61c6fe1194c1d8406e3c513481a98fc49a67e78
devizer <devizer@users.github.io>
2026-03-05 17:49:50 +0200

    brush up github release  [skip ci]

--- Universe.SqlServer.AdministrativeViews.CLI/BUILD-TOOL-And-STANDALONE-CLI/3) Create-GitHub-Release.cmd
+++ Universe.SqlServer.AdministrativeViews.CLI/BUILD-TOOL-And-STANDALONE-CLI/3) Create-GitHub-Release.cmd
@@ -2,12 +2,8 @@ call Set-Environment-Variables.cmd
 if Not Defined BUILD_SQLSERVER_ADMINISTRATIVE_VIEWS Goto :Err
 for /f %%i in (%BUILD_SQLSERVER_ADMINISTRATIVE_VIEWS%\PUBLIC\VERSION.TXT) do set VERSION=%%i
 
-echo SqlServer Administrative Views standalone self-contained release v%VERSION% > %BUILD_SQLSERVER_ADMINISTRATIVE_VIEWS%\Release-Notes.txt
-echo.  >> %BUILD_SQLSERVER_ADMINISTRATIVE_VIEWS%\Release-Notes.txt
-echo [SqlServer Administrative Views .NET tool]^(https://www.nuget.org/packages/SqlServer.AdministrativeViews#readme-body-tab^) is also available: >> %BUILD_SQLSERVER_ADMINISTRATIVE_VIEWS%\Release-Notes.txt
-echo ``` >> %BUILD_SQLSERVER_ADMINISTRATIVE_VIEWS%\Release-Notes.txt
-echo dotnet tool install --global SqlServer.AdministrativeViews >> %BUILD_SQLSERVER_ADMINISTRATIVE_VIEWS%\Release-Notes.txt
-echo ``` >> %BUILD_SQLSERVER_ADMINISTRATIVE_VIEWS%\Release-Notes.txt
+echo SqlServer Administrative Views standalone self-contained release for Windows, Linux and macOS v%VERSION% > %BUILD_SQLSERVER_ADMINISTRATIVE_VIEWS%\Release-Notes-1st-line.txt
+copy /b %BUILD_SQLSERVER_ADMINISTRATIVE_VIEWS%\Release-Notes-1st-line.txt + Github-Release-Notes-Body.md  %BUILD_SQLSERVER_ADMINISTRATIVE_VIEWS%\Release-Notes.txt
 
 gh release create -t "v%VERSION%" -F %BUILD_SQLSERVER_ADMINISTRATIVE_VIEWS%\Release-Notes.txt "v%VERSION%" %BUILD_SQLSERVER_ADMINISTRATIVE_VIEWS%\PUBLIC\*.*
 if ERRORLEVEL 1 Goto :err
---
+++ Universe.SqlServer.AdministrativeViews.CLI/BUILD-TOOL-And-STANDALONE-CLI/Github-Release-Notes-Body.md
@@ -0,0 +1,5 @@
+
+[SqlServer Administrative Views .NET tool](https://www.nuget.org/packages/SqlServer.AdministrativeViews#readme-body-tab) is also available:
+```
+dnx SqlServer.AdministrativeViews -- -o "Reports\{InstanceName} {Version} on {Platform}" -all
+```
